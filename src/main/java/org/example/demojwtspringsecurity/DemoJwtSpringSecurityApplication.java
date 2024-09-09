9b0f82a37dcb5aa0c4c67fd0b6876b18d9de640e
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-09-09 19:49:02 +0200

    init project

---
+++ src/main/java/org/example/demojwtspringsecurity/DemoJwtSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.demojwtspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJwtSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demojwtspringsecurity/DemoJwtSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demojwtspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
