70acf7f79e439e6c09de89f21ab04ab54010625f
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-09-13 13:39:49 +0200

    add AppUserService

---
+++ src/main/java/org/example/demojwtspringsecurity/service/jwt/AppUserService.java
@@ -0,0 +1,31 @@
+package org.example.demojwtspringsecurity.service.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.example.demojwtspringsecurity.entity.AppUser;
+import org.example.demojwtspringsecurity.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserService {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    public UserDetailsService getDetailsService() {
+
+        UserDetailsService detailsService = new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser user = appUserRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+                return user;
+            }
+        };
+
+        return detailsService;
+    }
+}
