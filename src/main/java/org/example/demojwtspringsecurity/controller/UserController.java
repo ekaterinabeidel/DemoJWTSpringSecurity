6bed7a251caf1091749bb110a0029742b4702f5e
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-09-13 13:45:22 +0200

    add UserController

---
+++ src/main/java/org/example/demojwtspringsecurity/controller/UserController.java
@@ -0,0 +1,17 @@
+package org.example.demojwtspringsecurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Ok");
+    }
+}
